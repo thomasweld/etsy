d44522735688335d74d0406dcbfcc80ca7c0c5e6
thomasweld <thomas.weld@gmail.com>
2016-04-08 10:59:43 -0400

    finished 1-4

--- js/main.js
+++ js/main.js
@@ -51,11 +51,76 @@ var itemTitles = itemsInPriceRange.map(function (item){
 var answer2Area = document.querySelector('#answer2');
 
 // Put our answer on the page + put each item on new line with .join
-answer2Area.innerHTML = itemTitles.join("<br />");;
+answer2Area.innerHTML = itemTitles.join("<br />");
 
 ////////////////////////////////////////////////////////////////////////////////
 
-// Which item has a "GBP" currency code? Display it's name and price.
+// 3. Which item has a "GBP" currency code? Display it's name and price.
+// from items.js --> "currency_code": "GBP",
+
+// creating a function to determine whether or not an item has a currency_code
+// of "GBP"
+function codeGBP(item){
+  return ( item.currency_code === "GBP" );
+}
+
+// Filtering the items array by way of the inPriceRange function from above
+var itemsWithCodeGBP = items.filter( codeGBP );
+
+// Mapping/storing the titles and prices of the objects / items in new array/variable
+// itemTitleAndPrice
+var itemTitleAndPrice = itemsWithCodeGBP.map(function (item){
+  return ( item.title + " " + item.price);
+});
+
+// joining each item in the array with a line break in between for HTML output
+var answerOutputFormatted = itemTitleAndPrice.join("<br />")
+
+// Find my "answer" area in the DOM
+var answer3Area = document.querySelector('#answer3');
+
+// Put our answer on the page + put each item on new line with .join
+answer3Area.innerHTML = answerOutputFormatted;
+
+////////////////////////////////////////////////////////////////////////////////
+
+// 4. Display a list of all items who are made of wood.
+
+// creating a function to determine whether or not an items materials = includes
+// "wood"
+function materialIsWood( item ){
+  return item.materials.indexOf("wood") > 0;
+}
+
+// Filtering the items array via materialIsWood function above
+var madeWithWood = items.filter( materialIsWood );
+
+// Mapping/storing the titles and prices of the objects / items in new array/variable
+// itemTitleAndPrice
+var itemTitlesWood = madeWithWood.map(function (item){
+  return ( item.title );
+});
+
+// Find my "answer" area in the DOM
+var answer4Area = document.querySelector('#answer4');
+
+// Put our answer on the page + put each item on new line with .join
+answer4Area.innerHTML = itemTitlesWood.join("<br />");
+
+////////////////////////////////////////////////////////////////////////////////
+
+// 5. Which items are made of eight or more materials? Display the name, number
+// of items and the items it is made of.
+
+
+
+
+
+
+
+////////////////////////////////////////////////////////////////////////////////
+
+// 6. How many items were made by their sellers?
 
 
 
@@ -66,6 +131,8 @@ answer2Area.innerHTML = itemTitles.join("<br />");;
 
 
 
+
+////////////////////////////////////////////////////////////////////////////////
 
 // // 1. Show me how to calculate the average price of all items.
 //
